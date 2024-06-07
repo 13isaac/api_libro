76f9b68573c8372ddbbce784ffde73214fdb540c
saxel0360 <isaacjoelillatarco@gmail.com>
2024-06-07 19:10:21 -0100

    adding minimal app

---
+++ app/tests/test_libro_controller_user.py
@@ -0,0 +1,50 @@
+def test_get_libros_as_user(test_client, user_auth_headers):
+    # El usuario con el rol de "user" debería poder obtener la lista de productos
+    response = test_client.get("/api/libros", headers=user_auth_headers)
+    assert response.status_code == 200
+    assert response.json == []
+
+
+def test_create_libro_as_user(test_client, user_auth_headers):
+    # El usuario con el rol de "user" no debería poder crear un producto
+    data = {"titulo": "Rebelion", "autor": "Cornejo", "edicion": "Segunda", "disponibilidad": False}
+    response = test_client.post("/api/libros", json=data, headers=user_auth_headers)
+    assert response.status_code == 403
+
+def test_create_libro(test_client, admin_auth_headers):
+    # El usuario con el rol de "admin" debería poder crear un nuevo producto
+    data = {
+        "titulo": "Principito",
+        "autor": "Guillermo",
+        "edicion": "Quinta",
+        "disponibilidad": False,
+    }
+    response = test_client.post("/api/libros", json=data, headers=admin_auth_headers)
+    assert response.status_code == 201
+    assert response.json["titulo"] == "Principito"
+    assert response.json["autor"] == "Guillermo"
+    assert response.json["edicion"] == "Quinta"
+    assert response.json["disponibilidad"] == False
+
+def test_get_libro_as_user(test_client, user_auth_headers):
+    # El usuario con el rol de "user" debería poder obtener un producto específico
+    # Este test asume que existe al menos un producto en la base de datos
+    response = test_client.get("/api/libros/1", headers=user_auth_headers)
+    assert response.status_code == 200
+    assert "titulo" in response.json
+    assert "autor" in response.json
+    assert "edicion" in response.json
+    assert "disponibilidad" in response.json
+
+
+def test_update_libro_as_user(test_client, user_auth_headers):
+    # El usuario con el rol de "user" no debería poder actualizar un producto
+    data = {"titulo": "Calculo 1", "autor": "Stward", "edicion": "Primera", "disponibilidad": True}
+    response = test_client.put("/api/libros/1", json=data, headers=user_auth_headers)
+    assert response.status_code == 403
+
+
+def test_delete_libro_as_user(test_client, user_auth_headers):
+    # El usuario con el rol de "user" no debería poder eliminar un producto
+    response = test_client.delete("/api/libros/1", headers=user_auth_headers)
+    assert response.status_code == 403
